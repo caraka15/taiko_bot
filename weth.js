97c20ae1e656d980bff3cb5d75eea360000c548a
Crxa <carakawidi07@gmail.com>
2024-10-13 11:50:45 +0700

    fix bug

--- weth.js
+++ weth.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { exec } = require("child_process");
 const util = require("util");
 const fs = require("fs");
@@ -9,9 +10,18 @@ const execPromise = util.promisify(exec);
 // Baca konfigurasi
 const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 
-async function runCommand(command) {
+// Fungsi untuk mendapatkan semua private keys dari .env
+function getPrivateKeys() {
+  return Object.keys(process.env)
+    .filter((key) => key.startsWith("PRIVATE_KEY_"))
+    .map((key) => process.env[key]);
+}
+
+async function runCommand(command, env) {
   try {
-    const { stdout, stderr } = await execPromise(command);
+    const { stdout, stderr } = await execPromise(command, {
+      env: { ...process.env, ...env },
+    });
     return {
       output: stdout,
       error: stderr,
@@ -30,42 +40,63 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-// Fungsi untuk mendapatkan waktu server saat ini
 function getCurrentServerTime() {
   return moment()
     .tz(config.timezone || "Asia/Jakarta")
     .format("YYYY-MM-DD HH:mm:ss");
 }
 
-// Fungsi utama
 async function main() {
   const iterations = config.iterations || 70;
   const interval = config.interval || 30;
+  const privateKeys = getPrivateKeys();
 
   for (let i = 0; i < iterations; i++) {
     console.log(
       `[${getCurrentServerTime()}] Iteration ${i + 1} of ${iterations}`
     );
 
-    console.log("Running deposit...");
-    const depositResult = await runCommand("node weth_deposit.js");
-    console.log(depositResult.output);
+    for (let j = 0; j < privateKeys.length; j++) {
+      const privateKey = privateKeys[j];
+      console.log(
+        `[${getCurrentServerTime()}] Processing wallet ${j + 1} of ${
+          privateKeys.length
+        }`
+      );
 
-    if (depositResult.retval === 0) {
-      console.log(`Deposit successful, waiting for ${interval} seconds...`);
-      await sleep(interval * 1000);
+      const env = { PRIVATE_KEY: privateKey };
 
-      console.log("Running withdraw...");
-      const withdrawResult = await runCommand("node weth_withdraw.js");
-      console.log(withdrawResult.output);
+      console.log("Running deposit...");
+      const depositResult = await runCommand("node weth_deposit.js", env);
+      console.log(depositResult.output);
 
-      if (withdrawResult.retval === 0) {
-        console.log("Withdraw successful.");
+      if (depositResult.retval === 0) {
+        console.log(
+          `Deposit successful for wallet ${
+            j + 1
+          }, waiting for ${interval} seconds...`
+        );
+        await sleep(interval * 1000);
+
+        console.log("Running withdraw...");
+        const withdrawResult = await runCommand("node weth_withdraw.js", env);
+        console.log(withdrawResult.output);
+
+        if (withdrawResult.retval === 0) {
+          console.log(`Withdraw successful for wallet ${j + 1}.`);
+        } else {
+          console.log(`Withdraw failed for wallet ${j + 1}.`);
+        }
       } else {
-        console.log("Withdraw failed.");
+        console.log(`Deposit failed for wallet ${j + 1}.`);
       }
-    } else {
-      console.log("Deposit failed.");
+
+      if (j < privateKeys.length - 1) {
+        console.log(
+          `Waiting for ${interval} seconds before processing next wallet...`
+        );
+        await sleep(interval * 1000);
+      }
     }
 
     if (i < iterations - 1) {
@@ -74,17 +105,24 @@ async function main() {
     }
   }
 
-  console.log(`[${getCurrentServerTime()}] All iterations completed.`);
+  console.log(
+    `[${getCurrentServerTime()}] All iterations completed for all wallets.`
+  );
 }
 
-// Fungsi untuk menjadwalkan tugas
 function scheduleTask() {
   const timezone = config.timezone || "Asia/Jakarta";
   const scheduledTime = config.scheduledTime || "07:00";
   const [scheduledHour, scheduledMinute] = scheduledTime.split(":").map(Number);
 
   console.log(`[${getCurrentServerTime()}] Current configuration:`);
-  console.log(JSON.stringify(config, null, 2));
+  console.log(
+    JSON.stringify(
+      { ...config, wallets: `${getPrivateKeys().length} wallets` },
+      null,
+      2
+    )
+  );
 
   console.log(
     `[${getCurrentServerTime()}] Scheduling task to run at ${scheduledTime} ${timezone}`
@@ -102,7 +140,6 @@ function scheduleTask() {
     `[${getCurrentServerTime()}] Task scheduled. Waiting for execution time...`
   );
 
-  // Hitung waktu hingga eksekusi berikutnya
   const now = moment().tz(timezone);
   const nextExecution = moment()
     .tz(timezone)
@@ -118,10 +155,8 @@ function scheduleTask() {
   );
 }
 
-// Jalankan penjadwalan
 scheduleTask();
 
-// Tangani sinyal terminasi untuk logging
 process.on("SIGINT", function () {
   console.log(`[${getCurrentServerTime()}] Script terminated.`);
   process.exit();
